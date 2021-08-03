645f298466497160d3470b92df72815ac7b408a9
furrutiav <74210202+furrutiav@users.noreply.github.com>
2021-08-03 15:01:21 -0400

    update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from flask_cors import CORS
+# from flask_cors import CORS
 import base64
 
 from models import solve
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
-api = Api(app)
+# CORS(app, resources={r"/*": {"origins": "*"}})
+# api = Api(app)
 
 
 def base64_to_img(my_string):
@@ -17,19 +17,28 @@ def base64_to_img(my_string):
         f.write(my_img)
 
 
-class Base(Resource):
-    def get(self):
-        return {"about": "hello world"}
+# class Base(Resource):
+#     def get(self):
+#         return {"about": "hello world"}
+#
+#     def post(self):
+#         some_json = request.get_json()
+#         my_string = some_json["test"]
+#         base64_to_img(my_string)
+#         performance = solve()
+#         return {"TEST": performance}, 201
 
-    def post(self):
-        some_json = request.get_json()
-        my_string = some_json["test"]
-        base64_to_img(my_string)
-        performance = solve()
-        return {"TEST": performance}, 201
 
+# api.add_resource(Base, "/")
 
-api.add_resource(Base, "/")
+@app.route('/', methods=['POST'])
+def output():
+    some_json = request.get_json()
+    my_string = some_json["test"]
+    base64_to_img(my_string)
+    performance = solve()
+    return {"TEST": performance}, 201
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
